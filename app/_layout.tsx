a7699edccd748e4b7c20a2136e86b741c3e95107
duy-nq <nqduy9207@gmail.com>
2025-03-06 17:08:08 +0700

    feat: enhance data fetching in RootLayout to include profile and others alongside emails

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,31 @@
 import { Slot } from "expo-router";
 import { useEffect } from "react";
 import { ScrollProvider } from "@/contexts/ScrollContextMail";
-import { useEmailStore } from "@/stores/email.store";
 import { ModalProvider } from "@/contexts/ModalContext";
-
+import { useMainStore } from "@/stores/main.store";
+import { useEmailStore } from "@/stores/email.store";
 export default function RootLayout() {
+	const { mainEmail, fGetOthers, fGetProfile } = useMainStore();
 	const { fGetEmails } = useEmailStore();
 
 	const fetchData = async () => {
-		await fGetEmails();
+    try {
+      await Promise.all([
+        fGetProfile(mainEmail),
+        fGetOthers(mainEmail)
+      ]);
+      
+      await fGetEmails();
+      
+      return true;
+    } catch (error) {
+      console.log('Login error:', error);
+      return false;
+    }
 	};
 
 	useEffect(() => {
-			fetchData();
+		fetchData();
 	}, []);
 	
 	return (
