4316e6ebe837088373071230f0ea9c912c71de3e
duy-nq <nqduy9207@gmail.com>
2025-03-06 14:31:02 +0700

    feat: integrate ProfileModal and update unread email count in tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,44 +1,46 @@
 import BottomNavigation from "@/components/commons/BottomNavigation";
-import { IONICONS_MAIL, IONICONS_MEET, IONICONS_MAIL_FOCUS, IONICONS_MEET_FOCUS, MATERIALICONS_CHAT, MATERIALICONS_CHAT_FOCUS } from "@/constants/iconConvension";
-import { StyleSheet, View } from "react-native";
+import { IONICONS_MAIL, IONICONS_MEET, IONICONS_MAIL_FOCUS, IONICONS_MEET_FOCUS } from "@/constants/iconConvension";
+import { Alert, Animated, Modal, StyleSheet, Text, View } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
-import MaterialIconsIcon from "react-native-vector-icons/MaterialIcons";
+import { useEmailStore } from "@/stores/email.store";
+import { Slot } from "expo-router";
+import { useModalContext } from "@/contexts/ModalContext";
+import ProfileModal from "@/components/modals/ProfileModal";
+import { useScrollContextMail } from "@/contexts/ScrollContextMail";
 
-const tabs = [
-    {
-        label: "Mail",
-        icon: <Icon name={IONICONS_MAIL} size={28} />,
-        iconFocused: <Icon name={IONICONS_MAIL_FOCUS} size={28} />,
-        to: "(mail)",
-        unread: 100,
-    },
-    {
-        label: "Chat",
-        icon: <MaterialIconsIcon name={MATERIALICONS_CHAT} size={28} />,
-        iconFocused: <MaterialIconsIcon name={MATERIALICONS_CHAT_FOCUS} size={28} />,
-        to: "(chat)",
-        unread: 2,
-    },
-    {
-        label: "Meet",
-        icon: <Icon name={IONICONS_MEET} size={28} />,
-        iconFocused: <Icon name={IONICONS_MEET_FOCUS} size={28} />,
-        to: "(meet)",
-        unread: 4,
-    },
-];
 
 export default function TabsLayout() {
+	const {emails} = useEmailStore();
+	const { tabBarTranslateY } = useScrollContextMail();
+	
+	const tabs = [
+		{
+			label: "Mail",
+			icon: <Icon name={IONICONS_MAIL} size={28} />,
+			iconFocused: <Icon name={IONICONS_MAIL_FOCUS} size={28} />,
+			to: "(mail)",
+			unread: emails.length || 0,
+		},
+		{
+			label: "Meet",
+			icon: <Icon name={IONICONS_MEET} size={28} />,
+			iconFocused: <Icon name={IONICONS_MEET_FOCUS} size={28} />,
+			to: "(meet)",
+			unread: 0,
+		},
+	];
+
 	return (
 		<View style={styles.container}>
-				<BottomNavigation tabs={tabs} />
+			<ProfileModal />
+			<BottomNavigation tabs={tabs} />
 		</View>
-	);
-};
+		);
+	};
 
-const styles = StyleSheet.create({
-	container: {
+	const styles = StyleSheet.create({
+		container: {
 			flex: 1,
 			backgroundColor: "white",
-	},
+		},
 });
