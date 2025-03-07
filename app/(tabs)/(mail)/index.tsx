de4b3763866115329601cbaa9ed3fe69e8fafd64
duy-nq <nqduy9207@gmail.com>
2025-03-07 17:27:12 +0700

    feat: update MailList component to use email store and improve rendering logic

--- app/(tabs)/(mail)/index.tsx
+++ app/(tabs)/(mail)/index.tsx
@@ -1,81 +1,99 @@
 import React, { useState } from "react";
-import { StyleSheet, RefreshControl, Animated, FlatList, Text } from "react-native";
+import {
+  StyleSheet,
+  RefreshControl,
+  Animated,
+  FlatList,
+  Text,
+} from "react-native";
 import IncomingMail from "@/components/mail/IncomingMail";
-import { COLOR_GOOGLE_BLUE, COLOR_GOOGLE_GREEN, COLOR_GOOGLE_RED, COLOR_GOOGLE_YELLOW } from "@/constants/color";
+import {
+  COLOR_GOOGLE_BLUE,
+  COLOR_GOOGLE_GREEN,
+  COLOR_GOOGLE_RED,
+  COLOR_GOOGLE_YELLOW,
+} from "@/constants/color";
 import { mockEmails } from "@/constants/mailData";
 import { useScrollContextMail } from "@/contexts/ScrollContextMail";
+import { useEmailStore } from "@/stores/email.store";
 
 const colors = ["#34A853", "#4285F4", "#EA4335", "#FBBC05"]; // Google colors
 
 export default function MailList() {
-    const [refreshing, setRefreshing] = useState(false);
-    const [spinnerColor] = useState(new Animated.Value(0)); // Animated value for color transition
+  const [refreshing, setRefreshing] = useState(false);
+  const [spinnerColor] = useState(new Animated.Value(0)); // Animated value for color transition
+  const { emails } = useEmailStore();
 
-    // Function to refresh mails
-    const onRefresh = () => {
-        setRefreshing(true);
-        startColorAnimation(); // Start animation
+  // Function to refresh mails
+  const onRefresh = () => {
+    setRefreshing(true);
+    startColorAnimation(); // Start animation
 
-        setTimeout(() => {
-            setRefreshing(false);
-        }, 1000);
-    };
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 1000);
+  };
 
-    // Function to animate spinner color
-    const startColorAnimation = () => {
-        Animated.loop(
-            Animated.timing(spinnerColor, {
-                toValue: colors.length - 1, // Cycle through all colors
-                duration: 1500,
-                useNativeDriver: false, // Required for backgroundColor animations
-            })
-        ).start();
-    };
+  // Function to animate spinner color
+  const startColorAnimation = () => {
+    Animated.loop(
+      Animated.timing(spinnerColor, {
+        toValue: colors.length - 1, // Cycle through all colors
+        duration: 1500,
+        useNativeDriver: false, // Required for backgroundColor animations
+      })
+    ).start();
+  };
 
-    const { scrollY } = useScrollContextMail();
+  const { scrollY } = useScrollContextMail();
 
-    return (
-        <FlatList 
-            data={mockEmails}
-            keyExtractor={item => item.content.toString()}
-            renderItem={({ item }) => (
-                <IncomingMail 
-                    key={item.timestamp}
-                    sender={item.sender}
-                    title={item.title}
-                    content={item.content}
-                    timestamp={item.timestamp}
-                    unread={item.unread}
-                    star={item.star}
-                />
-            )}
-            contentContainerStyle={styles.scrollViewContent}
-            onScroll={Animated.event(
-                [{ nativeEvent: { contentOffset: { y: scrollY } } }],
-                { useNativeDriver: false }
-            )}
-            refreshControl={
-                <RefreshControl 
-                    refreshing={refreshing}
-                    onRefresh={onRefresh}
-                    progressViewOffset={100}
-                    colors={[COLOR_GOOGLE_RED, COLOR_GOOGLE_BLUE, COLOR_GOOGLE_YELLOW, COLOR_GOOGLE_GREEN]}
-                />
-            }
-            ListHeaderComponent={
-                <Text style={{ textAlign: "left", padding: 12 }}>
-                    Important
-                </Text>
-            }
+  return (
+    <FlatList
+      data={emails}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <IncomingMail email={item} />}
+      contentContainerStyle={styles.scrollViewContent}
+      onScroll={Animated.event(
+        [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+        { useNativeDriver: false }
+      )}
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          progressViewOffset={100}
+          colors={[
+            COLOR_GOOGLE_RED,
+            COLOR_GOOGLE_BLUE,
+            COLOR_GOOGLE_YELLOW,
+            COLOR_GOOGLE_GREEN,
+          ]}
         />
-    );
-};
+      }
+      ListHeaderComponent={
+        <Text style={{ textAlign: "left", padding: 12 }}>Incoming Mails</Text>
+      }
+      ListEmptyComponent={
+        <Text
+          style={{
+            textAlign: "center",
+            padding: 12,
+            color: "#000",
+            fontSize: 24,
+          }}
+        >
+          It's empty here!
+        </Text>
+      }
+    />
+  );
+}
 
 const styles = StyleSheet.create({
-    scrollViewContent: {
-        flexGrow: 1,
-        paddingBottom: 90,
-        paddingTop: 80,
-        backgroundColor: '#fff',
-    },
+  scrollViewContent: {
+    flexGrow: 1,
+    paddingBottom: 90,
+    paddingTop: 80,
+    backgroundColor: "#fff",
+  },
 });
