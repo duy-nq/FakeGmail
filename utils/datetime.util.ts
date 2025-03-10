ad7f1e0b008963e5da77c50405b031ea3ba87d79
duy-nq <nqduy9207@gmail.com>
2025-03-10 10:59:30 +0700

    feat: add overviewExtendedDate function for enhanced date formatting

--- utils/datetime.util.ts
+++ utils/datetime.util.ts
@@ -38,4 +38,15 @@ export function formatEmailDate(timestamp: number): string {
     : diffDays < 31
     ? `${diffDays} days ago`
     : date.toLocaleDateString("en-GB");
+};
+
+export function overviewExtendedDate(timestamp: number) {
+  const date = new Date(timestamp);
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const day = date.getDate().toString()
+  const month = date.getMonth() + 1; // Months are 0-based
+  const year = date.getFullYear();
+
+  return `${hours}:${minutes} ${day} Th${month} ${year}`;
 }
