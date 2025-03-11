2c5758be63b2f5e70190299854566417252c3a53
duy-nq <nqduy9207@gmail.com>
2025-03-11 13:19:01 +0700

    feat: implement attachment upload and management in email editor

--- app/editor/index.tsx
+++ app/editor/index.tsx
@@ -17,14 +17,19 @@ import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import { useEmailStore } from "@/stores/email.store";
 import Email from "@/types/email.type";
 import "react-quill/dist/quill.snow.css";
+import * as DocumentPicker from "expo-document-picker";
+import File from "@/types/file.type";
 
 import formatTextToSave from "@/utils/convert.utitl";
+import Attachment from "@/components/mail/Attactment";
 
 export default function Editor() {
   const router = useRouter();
   const [recipient, setRecipient] = useState("");
   const [subject, setSubject] = useState("");
   const [body, setBody] = useState("");
+  const [attachments, setAttachments] = useState<File[]>([]);
+  const [bodyHeight, setBodyHeight] = useState(40);
 
   const { profile } = useMainStore();
   const { fSendEmail } = useEmailStore();
@@ -43,7 +48,7 @@ export default function Editor() {
       alert("Email body is required");
       return;
     }
-    
+
     const payload: Email = {
       id: "",
       sender: profile.email,
@@ -60,15 +65,49 @@ export default function Editor() {
       isStarred: false,
       isDelete: false,
       isDraft: false,
-    }
+    };
 
-    if (await fSendEmail(payload) != null) {
+    if ((await fSendEmail(payload)) != null) {
       alert("Email sent successfully");
       router.back();
     } else {
       alert("Failed to send email");
       return;
     }
+  };
+
+  const handleUploadAttachment = async () => {
+    try {
+      const result = await DocumentPicker.getDocumentAsync({
+        type: "*/*",
+        multiple: true,
+      });
+
+      if (result.canceled) {
+        return;
+      }
+
+      const attachments = result.assets.map(
+        (file) =>
+          ({
+            fileName: file.name,
+            uri: file.uri,
+            type: file.mimeType || "unknown",
+            size: file.size || 0,
+          } as File)
+      );
+
+      setAttachments(attachments);
+    } catch (error) {
+      console.log("Error uploading attachment", error);
+    }
+  };
+
+  const handleRemoveAttachment = (fileName: string) => {
+    const newAttachments = attachments.filter(
+      (file) => file.fileName !== fileName
+    );
+    setAttachments(newAttachments);
   };
 
   return (
@@ -84,7 +123,10 @@ export default function Editor() {
           </TouchableOpacity>
 
           <View style={styles.headerActions}>
-            <TouchableOpacity style={styles.headerButton}>
+            <TouchableOpacity
+              style={styles.headerButton}
+              onPress={handleUploadAttachment}
+            >
               <Icon name="attach" size={24} color="#5f6368" />
             </TouchableOpacity>
             <TouchableOpacity style={styles.headerButton}>
@@ -133,17 +175,32 @@ export default function Editor() {
             placeholder="Subject"
           />
 
-          <View style={styles.divider} />
-
           {/* Email body */}
           <TextInput
-            style={styles.bodyInput}
+            style={[styles.bodyInput, { height: bodyHeight }]}
+            onContentSizeChange={(e) => {
+              const newHeight = e.nativeEvent.contentSize.height;
+              setBodyHeight(newHeight); // Ensure it shrinks back
+            }}
             value={body}
             onChangeText={setBody}
             placeholder="Compose email"
-            multiline
+            multiline={true}
             textAlignVertical="top"
           />
+
+          <View
+            style={{
+              flexDirection: "row",
+              gap: 8,
+              flexWrap: "wrap",
+              marginVertical: 12,
+            }}
+          >
+            {attachments.map((file, index) => {
+              return <Attachment key={index} file={file} onRemove={handleRemoveAttachment} />;
+            })}
+          </View>
         </ScrollView>
       </KeyboardAvoidingView>
     </SafeAreaView>
@@ -154,6 +211,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
+    width: "100%",
   },
   keyboardAvoidingView: {
     flex: 1,
@@ -184,7 +242,7 @@ const styles = StyleSheet.create({
     gap: 12,
   },
   inputLabel: {
-    width: 40, 
+    width: 40,
     fontSize: 16,
     color: "#5f6368",
   },
@@ -223,12 +281,12 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
   },
   bodyInput: {
-    flex: 1,
     fontSize: 16,
     color: "#202124",
     paddingHorizontal: 16,
     paddingVertical: 12,
-    minHeight: 200,
+    minHeight: 40,
+    maxHeight: "40%",
   },
   formatBar: {
     flexDirection: "row",
